23072270da5e6d00c29f220da466eeaf8625c8e2
OkruzhnovT <timokruzhnov4@gmail.com>
2024-10-25 13:49:39 +0300

    insert cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,16 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld dub =
+                applicationContext.getBean("helloWorld", HelloWorld.class);
+        HelloWorld elka =
+                applicationContext.getBean("helloWorld", HelloWorld.class);
+        Cat barsik =
+                applicationContext.getBean("cat", Cat.class);
+        Cat vasya =
+                applicationContext.getBean("cat", Cat.class);
+        System.out.println(dub == elka);
+
+        System.out.println(barsik == vasya);
     }
 }
